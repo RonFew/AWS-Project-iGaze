9d6337e6c36acbfd5a832b849d592903a431c702
ron <ron3few@gmail.com>
2025-12-26 12:00:25 -0500

    my local changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import cv2 # image processing library 
 import numpy # scientific and numerical computing
 import dlib # face recognition
@@ -49,4 +50,76 @@ while True:
         break
 
 cam.release()
+=======
+import cv2 # image processing library 
+import numpy # scientific and numerical computing
+import dlib # face recognition
+from math import hypot
+
+
+REC_COLOR = (0, 255, 0,)
+REC_THIC = 1
+ESC = 27 #ascii for escape on keyboard
+
+L_EYE_MARKS = [36, 37, 38, 39, 40, 41]
+R_EYE_MARKS = [42, 43, 44, 45, 46, 47]
+BLINK_VAL = 8
+
+FONT = cv2.FONT_HERSHEY_SCRIPT_COMPLEX
+
+cam = cv2.VideoCapture(0) #web camera capture
+detector = dlib.get_frontal_face_detector() #detect face
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat") #collect eye location using .dat file
+
+def midpoint(p1,p2):
+    return int((p1.x + p2.x) / 2), int((p1.y + p2.y) / 2) # return the exact mid between two, can't be dec
+
+def get_blinking_ratio(eye_points, facial_landmarks):
+    left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y) # detect left end of eye
+    right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y) # detect right end of eye
+
+    center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2])) # detect mid of top part of eye
+    center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4])) # detect mid of bottom part of eye
+
+    hor_line = cv2.line(frame, left_point, right_point, REC_COLOR,  REC_THIC) # draw a line between ends of eye
+    ver_line = cv2.line(frame, center_top, center_bottom, REC_COLOR,  REC_THIC) # draw a line between ends of eye
+
+    hor_line_lenght = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1])) # cal length between sided fo eye
+    ver_line_lenght = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1])) # cal length between bottom and top
+
+    return hor_line_lenght / ver_line_lenght # ratio, blinking value comperison
+
+
+while True:
+    _, frame = cam.read() # _ indicate that a value returned by a function is being intentionally ignored.
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # gray sacle saves computing power
+
+    faces = detector(gray) #arry of faces detects location of face in 2d space
+    for face in faces:
+        cv2.rectangle(frame, (face.left(), face.top()), (face.right(), face.bottom()), REC_COLOR, REC_THIC) # surround face
+
+        #blink detection
+        landmarks = predictor(gray, face) # specific marks on face procesed
+        left_eye_ratio = get_blinking_ratio(L_EYE_MARKS, landmarks)
+        right_eye_ratio = get_blinking_ratio(R_EYE_MARKS, landmarks)
+        blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
+        print(blinking_ratio)
+
+        if blinking_ratio > BLINK_VAL: 
+            cv2.putText(frame, "BLINK", (50, 150), FONT, REC_THIC, REC_COLOR) #show indication when blinking
+
+    #show camera
+    mirrored_frame = cv2.flip(frame, 1) # Flip the frame horizontally (mirror effect) 
+    cv2.imshow("Camera", mirrored_frame) # will popup a window named "Camera" 
+ 
+    #end sesssion
+    key = cv2.waitKey(1) # if 'ESC' on keyboard is pressed, exit web cam
+    if key  == ESC:
+        break
+
+    elif cv2.getWindowProperty("Camera", cv2.WND_PROP_VISIBLE) < 1: # Check if the window was closed via the "X" button
+        break
+
+cam.release()
+>>>>>>> 3e2b3e8 (initial commit)
 cv2.destroyAllWindows() 
